21e8e02f54640a4fdd579dc62bdb2b7f4ab24af6
davidbarone <davidbarone@live.com>
2024-03-03 11:37:12 +1100

    CsvWriter tests

--- Dbarone.Net.Csv.Tests/Csv/CsvWriter.Tests.cs
+++ Dbarone.Net.Csv.Tests/Csv/CsvWriter.Tests.cs
@@ -14,7 +14,16 @@ namespace Dbarone.Net.Csv.Tests;
 public class CsvWriterTests
 {
 
-    public IEnumerable<StringDictionary> GetSimpleStringDictionary()
+    public IEnumerable<StringDictionary> GetMultilineRecord(string lineSeparator)
+    {
+        StringDictionary sd = new StringDictionary();
+        sd.Add("foo", $"123{lineSeparator}aaa");
+        sd.Add("bar", "456");
+        sd.Add("baz", "789");
+        yield return sd;
+    }
+
+    public IEnumerable<StringDictionary> GetOneRecord()
     {
         StringDictionary sd = new StringDictionary();
         sd.Add("foo", "123");
@@ -23,12 +32,67 @@ public class CsvWriterTests
         yield return sd;
     }
 
+    public IEnumerable<StringDictionary> GetTwoRecords()
+    {
+        StringDictionary sd = new StringDictionary();
+        sd.Add("foo", "123");
+        sd.Add("bar", "456");
+        sd.Add("baz", "789");
+        yield return sd;
+
+        sd = new StringDictionary();
+        sd.Add("foo", "aaa");
+        sd.Add("bar", "bbb");
+        sd.Add("baz", "ccc");
+        yield return sd;
+    }
+
     [Theory]
     [InlineData(',', '"', "\r\n", "foo,bar,baz\r\n123,456,789\r\n")]
     [InlineData('|', '"', ";;", "foo|bar|baz;;123|456|789;;")]
-    public void TestValidWrite(char fieldSeparator, char fieldEscape, string lineDelimiter, string expected)
+    public void TestValidWrite1(char fieldSeparator, char fieldEscape, string lineDelimiter, string expected)
     {
-        var sd = GetSimpleStringDictionary();
+        var sd = GetOneRecord();
+        CsvConfiguration configuration = new CsvConfiguration();
+        configuration.FieldEscape = fieldEscape;
+        configuration.FieldSeparator = fieldSeparator;
+        configuration.LineSeparator = lineDelimiter;
+        configuration.Headers = new string[] { "foo", "bar", "baz" };
+
+        MemoryStream ms = new MemoryStream();
+        CsvWriter csv = new CsvWriter(ms, configuration);
+        csv.Write(sd);
+
+        var actual = Encoding.UTF8.GetString(ms.ToArray());
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(',', '"', "\r\n", "foo,bar,baz\r\n123,456,789\r\naaa,bbb,ccc\r\n")]
+    [InlineData('|', '"', ";;", "foo|bar|baz;;123|456|789;;aaa|bbb|ccc;;")]
+    public void TestValidWrite2(char fieldSeparator, char fieldEscape, string lineDelimiter, string expected)
+    {
+        var sd = GetTwoRecords();
+        CsvConfiguration configuration = new CsvConfiguration();
+        configuration.FieldEscape = fieldEscape;
+        configuration.FieldSeparator = fieldSeparator;
+        configuration.LineSeparator = lineDelimiter;
+        configuration.Headers = new string[] { "foo", "bar", "baz" };
+
+        MemoryStream ms = new MemoryStream();
+        CsvWriter csv = new CsvWriter(ms, configuration);
+        csv.Write(sd);
+
+        var actual = Encoding.UTF8.GetString(ms.ToArray());
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(',', '"', "\r\n", "foo,bar,baz\r\n\"123\r\naaa\",456,789\r\n")]
+    [InlineData('|', '"', ";;", "foo|bar|baz;;\"123;;aaa\"|456|789;;")]
+    public void TestMultiLineWrite(char fieldSeparator, char fieldEscape, string lineDelimiter, string expected)
+    {
+        var sd = GetMultilineRecord(lineDelimiter);
         CsvConfiguration configuration = new CsvConfiguration();
         configuration.FieldEscape = fieldEscape;
         configuration.FieldSeparator = fieldSeparator;
